db9689be0e07dcc824890947c41a4e7a4090b42f
olayinka bello <olayinkabello962@gmail.com>
2022-04-13 15:14:13 +0100

    finish designing the login page and set up some routuing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Index from './pages/index';
+import Signup from './pages/signup';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path='/' element={ <Index /> } />
+        <Route path='/signup' element={ <Signup /> } />
       </Routes>
     </>
   );
