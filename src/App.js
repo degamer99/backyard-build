5b9e4bc2defc096cf6957824f0760907bd2dacb6
degamer99 <75088933+degamer99@users.noreply.github.com>
2023-04-22 21:17:52 +0100

    Update App.js
    added adsense

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import Index from './pages/index';
 import Signup from './pages/signup';
 
 function App() {
+  useEffect(() => {
+  const script = document.createElement('script');
+
+  script.src = "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5756583337185960";
+  script.crossOrigin="anonymous"
+  script.async = true;
+
+  document.body.appendChild(script);
+
+//   return () => {
+//     document.body.removeChild(script);
+//   }
+}, []);
   return (
     <>
       <Routes>
